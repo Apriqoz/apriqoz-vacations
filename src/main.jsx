faf735d8bc0a913495abc677d59602dddf47ce62
Apriqoz <apriqoz@gmail.com>
2026-09-21 17:04:51 +0530

    Fix GitHub Pages route refreshes

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+
+const params = new URLSearchParams(window.location.search);
+const redirectPath = params.get("p");
+
+if (redirectPath) {
+	const redirectQuery = params.get("q") || "";
+	const redirectHash = params.get("h") || "";
+	window.history.replaceState(null, "", `${redirectPath}${redirectQuery}${redirectHash}`);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
